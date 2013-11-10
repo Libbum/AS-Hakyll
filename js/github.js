996e129be1fa18d7c823596e6ffaf21dbd4970a1
Tim DuBois <genesis@neophilus.net>
2013-11-10 18:11:43 +1100

    github status in sidebar

---
+++ js/github.js
@@ -0,0 +1,16 @@
+//Quick and easy recent github activity without jQuery. 
+//Uses Github's v3 API and JSON-P
+//Author: Thim DuBois 10/11/13
+
+window.repolist = function(response) {
+    var data = response.data
+    var i = 0, fragment = '<ul>';
+
+    for (i; i < 4; i++) {
+        fragment += '<li><a href="'+data[i].html_url+'">'+data[i].name+'</a> ('+data[i].language+')<br>'+data[i].description+'</li>';
+    }
+
+    document.getElementById("github").innerHTML = fragment+'</ul>';
+};
+
+
--- site.hs
+++ site.hs
@@ -71,7 +71,7 @@ main = hakyllWith config $ do
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- (take 5) <$> (recentFirst =<< loadAll "posts/*")
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
                     constField "title" "Home"                `mappend`
