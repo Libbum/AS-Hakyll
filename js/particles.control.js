8e6ab756f44aa92cfafceb7ee79b5c62511084e3
Tim DuBois <timothy.dubois@rmit.edu.au>
2014-11-13 19:16:51 +1100

    Particle field

---
+++ js/particles.control.js
@@ -0,0 +1,34 @@
+particlesJS('particles-js', {
+    particles: {
+        color: '#777',
+        shape: 'circle',
+        opacity: 0.4,
+        size: 2.5,
+        size_random: true,
+        nb: 150,
+        line_linked: {
+            enable_auto: true,
+            distance: 250,
+            color: '#777',
+            opacity: 0.4,
+            width: 1,
+            condensed_mode: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 600
+            }
+        },
+        anim: {
+            enable: true,
+            speed: 0.25
+        }
+    },
+    interactivity: {
+        enable: true,
+        mouse: {
+            distance: 200
+        },
+        mode: 'grab'
+    },
+    retina_detect: true
+});
