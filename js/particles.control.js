278b0a7f909fa662369265484be0272dcdd0c43f
Tim DuBois <timothy.dubois@rmit.edu.au>
2014-11-22 15:33:01 +1100

    Style fixes for mobile

--- js/particles.control.js
+++ js/particles.control.js
@@ -1,34 +1,36 @@
-particlesJS('particles-js', {
-    particles: {
-        color: '#777',
-        shape: 'circle',
-        opacity: 0.5,
-        size: 2.5,
-        size_random: true,
-        nb: 100,
-        line_linked: {
-            enable_auto: true,
-            distance: 300,
-            color: '#777',
-            opacity: 0.4,
-            width: 1,
-            condensed_mode: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 600
-            }
-        },
-        anim: {
-            enable: true,
-            speed: 0.25
-        }
-    },
-    interactivity: {
-        enable: true,
-        mouse: {
-            distance: 200
-        },
-        mode: 'grab'
-    },
-    retina_detect: false
-});
+if (skel.isActive('desktop')) {
+  particlesJS('particles-js', {
+      particles: {
+          color: '#777',
+          shape: 'circle',
+          opacity: 0.5,
+          size: 2.5,
+          size_random: true,
+          nb: 100,
+          line_linked: {
+              enable_auto: true,
+              distance: 300,
+              color: '#777',
+              opacity: 0.4,
+              width: 1,
+              condensed_mode: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 600
+              }
+          },
+          anim: {
+              enable: true,
+              speed: 0.25
+          }
+      },
+      interactivity: {
+          enable: true,
+          mouse: {
+              distance: 200
+          },
+          mode: 'grab'
+      },
+      retina_detect: false
+  });
+}
