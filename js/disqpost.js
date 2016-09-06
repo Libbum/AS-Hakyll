972d9a34dd1fc805a6bf45da940ac0acc7d9e7ac
Tim DuBois <tim@neophilus.net>
2016-09-06 19:23:54 +0200

    Fixes #4. Although we the disqus embeds don't allow SRI protection it seems...

--- js/disqpost.js
+++ js/disqpost.js
@@ -1,7 +1,8 @@
  var disqus_shortname = 'axiomatic';
- var disqus_identifier = '$url$';
- var disqus_url = 'https://axiomatic.neophilus.net' + '$url$';
- var disqus_title = '$title$';
+ var url = document.getElementById("url").innerText;
+ var disqus_identifier = url;
+ var disqus_url = 'https://axiomatic.neophilus.net' + url;
+ var disqus_title = document.getElementById("title").innerText;
  (function() {
      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
      dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
